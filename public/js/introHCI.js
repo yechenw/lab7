573f66ae411bc34c898edc333c7092b1932ba9d6
Yechen Wei <yew019@ucsd.edu>
2020-02-19 15:50:52 -0800

    1

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(clickHandler);
+}
+
+function clickHandler() {
+	ga('send', 'event', 'like', 'click');
 }
